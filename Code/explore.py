45644a07349bf9115f379ab7d402e1904b78f798
K-Ellis <kieron.ellis@ucdconnect.ie>
2017-06-06 20:15:31 +0100

    update

---
+++ Code/explore.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("../Data/bank-additional_normalized.csv")
+
+print(len(df))
+
+df = pd.read_csv("../Data/bank-additional.csv")
+
+print(len(df))
+
+df = pd.read_csv("../Data/bank-full.csv")
+
+print(len(df))
